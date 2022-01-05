7ad3b026c3992de560d4f7d4fdf186b3532e1664
Ashitosh27 <rathodashitosh@gmail.com>
2022-01-05 14:46:36 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
+  let list=[1,1,1,1,1,1];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container-fluid">
+      <div className="row bg-secondary">
+        <div className="col">
+            <h1 className="d-flex justify-content-center">Mini-Project-React</h1>
+        </div>
+      </div>
+     
+      <div className="row">
+      {list.map((item)=>(
+        <div className="col-4 d-flex justify-content-center">
+          <div className="card mb-2">
+            <div className="card-body">
+              <img src="https://picsum.photos/400/401" alt="" />
+            </div>
+            <div className="card-footer m-2">
+                <h4 style={{backgroundColor:"skyblue",color:"white"}}>Happy New Year</h4>
+            </div>
+          </div>
+        </div>))}
+      </div>
     </div>
   );
 }
-
-export default App;
